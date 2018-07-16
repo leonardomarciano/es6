8ba4a1a321bd65a44a79c09241061c647020da3b
leonardomarciano <leonardo@marciano.io>
2018-07-16 11:54:27 -0300

    ensiando ts

---
+++ app/playground.js
@@ -0,0 +1,8 @@
+var myString = 'abc';
+var myNumber = 12;
+var myBoolean = true;
+var myArray = ['A', 'B', 'C', 'D'];
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myArray);
---
+++ app/playground.ts
@@ -0,0 +1,9 @@
+const myString:string = 'abc'
+const myNumber:number = 12
+const myBoolean:boolean = true
+const myArray:string[] = ['A', 'B', 'C', 'D']
+
+console.log(myString)
+console.log(myNumber)
+console.log(myBoolean)
+console.log(myArray)
